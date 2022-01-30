6e30b76894ae9db9b2ae47dc04ba168aa4721d01
yujidate990 <yuji.date.gh@gmail.com>
2022-01-30 20:32:06 +0900

    静的ページへ遷移する処理を追加

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
-import QRCode from "qrcode";
+// import QRCode from "qrcode";
 import "./reset.css";
+import "./font.css"
 import "./style.css";
 
 interface Data {
+  id?: string;
   title: string;
   next?: Data[];
   url?: string;
@@ -12,7 +14,7 @@ interface Data {
   reservation?: string;
 }
 
-const data: Data[] = [
+const db: Data[] = [
   {
     title: "MNP予約番号関連",
     next: [
@@ -20,6 +22,7 @@ const data: Data[] = [
         title: "キャリア",
         next: [
           {
+            id: "docomo",
             title: "Docomo",
             url: "https://www.nttdocomo.co.jp/mydocomo/",
             methodTitle: "MNP予約番号の手順（2021/12/25更新）",
@@ -29,6 +32,7 @@ const data: Data[] = [
                         主回線と子回線を反転させるようにオペレーターに依頼するべし。`,
           },
           {
+            id: "softbank",
             title: "au",
             url: "https://www.nttdocomo.co.jp/mydocomo/",
             methodTitle: "MNP予約番号の手順（2021/12/25更新）",
@@ -120,67 +124,89 @@ const data: Data[] = [
   },
 ];
 
-const a = (data: Data[], history: Data[][]) => {
-  const ul = document.querySelector("main > ul")!;
-  ul.innerHTML = "";
-  const info = document.querySelector(".info")!;
-  info.innerHTML = "";
 
-  const back = document.querySelector("#back")!;
+const ramen = (data: Data, history: Data[]) => {
+  const main = document.querySelector("main")!;
+  main.innerHTML = "";
+
+  const saisho = document.createElement("button");
+  saisho.innerHTML = "最初に戻る";
+  main.appendChild(saisho);
+
+  saisho.addEventListener("click", () => {
+    ramen({title: "root", next: db}, []);
+  });
+
+  const back = document.createElement("button")!;
+  back.id = "back";
+  back.innerHTML = "戻る"
+  main.appendChild(back);
+
+  const ul = document.createElement("ul");
+  main.appendChild(ul);
+
+  const info = document.createElement("div");
+  info.classList.add("info");
+  main.appendChild(info);
 
   if (history.length !== 0) {
     back.addEventListener("click", () => {
-      a(history[history.length - 1], history.slice(0, history.length - 2));
+      ramen(history[history.length - 1], history.slice(0, history.length - 1));
     });
   }
 
-  data.forEach((v) => {
-    // リスト
-    const li = document.createElement("li");
-    // ボタン
-    const button = document.createElement("button");
-    button.classList.add("menu-btn")
-    li.appendChild(button);
-    button.innerHTML = v.title;
+  if (data.next) {
+    data.next.forEach((v) => {
+      // リスト
+      const li = document.createElement("li");
+      // ボタン
+      const button = document.createElement("button");
+      button.classList.add("menu-btn")
+      li.appendChild(button);
+      button.innerHTML = v.title;
+  
+      button.addEventListener("click", () => {
+        ramen(v, [...history, data]);
+      });
+      ul.appendChild(li);
+    });  
+  } else {
+    // idが設定されていない場合はルートに遷移する
+    if (data.id) {
+      window.location.href = `/pages/${data.id}.html`;
+    } else {
+      window.location.href = "/";
+    }
+    // ul.innerHTML = "";
+  
+    // if (data.url) {
+    //   const url = document.createElement("a");
+    //   url.innerHTML = data.url;
+    //   info.appendChild(url);
 
-    button.addEventListener("click", () => {
-      history.push(data);
-      if (v.next) {
-        a(v.next, history);
-      } else {
-        ul.innerHTML = "";
+    //   const qrcode = document.createElement("canvas");
+    //   info.appendChild(qrcode);
+    //   QRCode.toCanvas(qrcode, data.url);
+    // }
 
-        if (v.url) {
-          const url = document.createElement("a");
-          url.innerHTML = v.url;
-          info.appendChild(url);
+    // if (data.description) {
+    //   const description = document.createElement("p");
+    //   description.innerHTML = data.description;
+    //   info.appendChild(description);
+    // }
 
-          const qrcode = document.createElement("canvas");
-          info.appendChild(qrcode);
-          QRCode.toCanvas(qrcode, v.url);
-        }
+    // if (data.methodTitle) {
+    //   const methodTitle = document.createElement("h3");
+    //   methodTitle.innerHTML = data.methodTitle;
+    //   info.appendChild(methodTitle);
+    // }
 
-        if (v.description) {
-          const description = document.createElement("p");
-          description.innerHTML = v.description;
-          info.appendChild(description);
-        }
-
-        if (v.methodTitle) {
-          const methodTitle = document.createElement("h3");
-          methodTitle.innerHTML = v.methodTitle;
-          info.appendChild(methodTitle);
-        }
-
-        if (v.method) {
-          const method = document.createElement("p");
-          method.innerHTML = v.method;
-          info.appendChild(method);
-        }
-      }
-    });
-    ul.appendChild(li);
-  });
+    // if (data.method) {
+    //   const method = document.createElement("p");
+    //   method.innerHTML = data.method;
+    //   info.appendChild(method);
+    // }
+  }
 };
 
-a(data, []);
+ramen({title: "root", next: db}, []);
